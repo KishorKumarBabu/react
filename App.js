00b056733ed033b7e0cd8ac8b266b4be94eb1a2b
kishor kumar <kishorkumarbabu5@gmail.com>
2025-04-23 21:21:47 +0530

    react

--- App.js
+++ App.js
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom/client"; // Use "react-dom/client" for React 18+
 const heading=React.createElement("h1",{id:"heading"},"kishor is here!")
 const root=ReactDOM.createRoot(document.getElementById("root"))
 /* jsx heading */
-const jsxheading=<h1 id="heading">kishor is here by jsx</h1>
+const jsxheading=<h1 id="heading" className="root">kishor is here by jsx</h1>
 root.render(jsxheading)
